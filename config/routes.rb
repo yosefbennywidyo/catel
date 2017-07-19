a5927b055ef9608be4196143710742de5bb64c99
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-07-19 02:39:00 +0000

    Add surat_keluar as resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'beranda/index'
   
+  resources :surat_keluar
+  
   root 'beranda#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
