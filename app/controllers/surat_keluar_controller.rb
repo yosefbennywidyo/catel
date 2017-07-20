9538a2d2b5ac4f6b5fc98a01665c7245b8f89daa
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-07-20 06:15:16 +0000

    Factored out into its own method so it can be reused by multiple actions in the same controller

--- app/controllers/surat_keluar_controller.rb
+++ app/controllers/surat_keluar_controller.rb
@@ -7,12 +7,19 @@ class SuratKeluarController < ApplicationController
     # render plain: params[:surat_keluar].inspect
     
     # Define @surat_keluar as new SuratKeluar
-    @surat_keluar = SuratKeluar.new(params.require(:surat_keluar).permit(:keterangan, :lampiran))
+    @surat_keluar = SuratKeluar.new(surat_keluar_params)
     
     # Saving data in the database
     @surat_keluar.save
     
     # Redirect the user to the show action
     redirect_to @surat_keluar
+  end
+  
+  # the method is made private to make sure it can't be called outside its intended context
+  # factored out into its own method so it can be reused by multiple actions in the same controller
+  private
+  def surat_keluar_params
+    params.require(:surat_keluar).permit(:keterangan, :lampiran)
   end
 end
