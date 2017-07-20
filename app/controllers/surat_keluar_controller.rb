014f5f1c6c72622ca01e2f33f089d5defdc84590
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-07-20 06:09:07 +0000

    Allow and require the title and text parameters for valid use of create

--- app/controllers/surat_keluar_controller.rb
+++ app/controllers/surat_keluar_controller.rb
@@ -7,7 +7,7 @@ class SuratKeluarController < ApplicationController
     # render plain: params[:surat_keluar].inspect
     
     # Define @surat_keluar as new SuratKeluar
-    @surat_keluar = SuratKeluar.new(params[:surat_keluar])
+    @surat_keluar = SuratKeluar.new(params.require(:surat_keluar).permit(:keterangan, :lampiran))
     
     # Saving data in the database
     @surat_keluar.save
