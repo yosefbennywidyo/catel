ee2ccb75b101209f42ff4222d88762e679d1e996
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-07-24 04:59:18 +0000

    Migrate AddStatusToSuratKeluar

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170721055957) do
+ActiveRecord::Schema.define(version: 20170724045523) do
 
   create_table "surat_keluars", force: :cascade do |t|
     t.text "keterangan"
     t.text "lampiran"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status", default: "konsep"
   end
 
   create_table "users", force: :cascade do |t|
