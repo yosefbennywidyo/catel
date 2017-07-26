91d57a01c87591990f5cd6d6b48ce7d7b5853662
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-07-26 08:26:44 +0000

    Fix data type and default value

--- db/migrate/20170724045523_add_status_to_surat_keluar.rb
+++ db/migrate/20170724045523_add_status_to_surat_keluar.rb
@@ -1,5 +1,5 @@
 class AddStatusToSuratKeluar < ActiveRecord::Migration[5.1]
   def change
-    add_column :surat_keluars, :status, :integer, default: 0
+    add_column :surat_keluars, :status, :string, default: "konsep"
   end
 end
