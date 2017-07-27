8dd7c59388c31618bf70ddb9362980a99c636dee
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-07-27 02:23:30 +0000

    Add verification for lampiran

--- app/models/surat_keluar.rb
+++ app/models/surat_keluar.rb
@@ -2,4 +2,6 @@ class SuratKeluar < ApplicationRecord
   validates :keterangan, presence: true,
                     length: { minimum: 15 }
   belongs_to :user
+  has_attached_file :lampiran
+  validates_attachment :lampiran, :content_type => {:content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document)}
 end
