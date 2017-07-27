1f1b92967798f0f26c6f3781ad18094501eac7ea
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-07-27 10:17:39 +0000

    Prepare for Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170727004329) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "surat_keluars", force: :cascade do |t|
     t.text "keterangan"
     t.text "lampiran"
